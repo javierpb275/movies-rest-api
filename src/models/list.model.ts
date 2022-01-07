31957bb59c4bdb0db0fe4a0793b44d451a069eba
javierpb275 <javierpb275@gmail.com>
2022-01-07 18:20:32 +0100

    created models list and user

---
+++ src/models/list.model.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import { IList } from "../types";
+
+const listSchema = new mongoose.Schema<IList>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    movies: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Movie",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model<IList>("List", listSchema);
---
+++ src/models/user.model.ts
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+import { IUser } from "../types";
+import validator from "validator";
+
+const userSchema = new mongoose.Schema<IUser>(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      lowercase: true,
+      validate(value: string) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is invalid");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 8,
+      validate(value: string) {
+        const strongRegex: RegExp = new RegExp(
+          "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"
+        );
+        if (!value.match(strongRegex)) {
+          throw new Error("Password is not secure");
+        }
+      },
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    lists: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "List",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model<IUser>("User", userSchema);
