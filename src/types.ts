94a0841773c1883c390e7466a139784034b437f3
javierpb275 <javierpb275@gmail.com>
2022-01-07 14:11:35 +0100

    changed properties of interfaces

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { ObjectId } from "mongoose";
 
 //MOVIE
 
@@ -11,8 +11,8 @@ export interface IMovie extends mongoose.Document {
   screenWriters: string[];
   genres: string[];
   synopsis: string;
-  reviews: IReview[]; //HAS MANY REVIEWS
-  scores: IScore[]; //HAS MANY SCORES
+  reviews: ObjectId[]; //HAS MANY REVIEWS
+  scores: ObjectId[]; //HAS MANY SCORES
 }
 
 //USER
@@ -22,16 +22,15 @@ export interface IUser extends mongoose.Document {
   password: string;
   username: string;
   isAdmin: boolean;
-  token?: string;
-  lists: IList[]; //HAS MANY LISTS OF MOVIES
+  lists: ObjectId[]; //HAS MANY LISTS OF MOVIES
 }
 
 //LIST
 
 export interface IList extends mongoose.Document {
   name: string;
-  movies: IMovie[]; //HAS MANY MOVIES
-  user: IUser; //IS MADE BY SOME USER
+  movies: ObjectId[]; //HAS MANY MOVIES
+  user: ObjectId; //IS MADE BY SOME USER
 }
 
 //REVIEW
@@ -41,8 +40,8 @@ type Opinion = "bad" | "mediocre" | "good";
 export interface IReview extends mongoose.Document {
   comment: string;
   opinion: Opinion;
-  movie: IMovie; //BELONGS TO SOME MOVIE
-  user: IUser; //IS MADE BY SOME USER
+  movie: ObjectId; //BELONGS TO SOME MOVIE
+  user: ObjectId; //IS MADE BY SOME USER
 }
 
 //SCORE
@@ -51,6 +50,6 @@ type Points = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
 
 export interface IScore extends mongoose.Document {
   points: Points;
-  movie: IMovie; //BELONGS TO A MOVIE
-  user: IUser; //IS MADE BY SOME USER
+  movie: ObjectId; //BELONGS TO A MOVIE
+  user: ObjectId; //IS MADE BY SOME USER
 }
