b9bdff805bd4d3628a191d5a3116ae455a538052
javierpb275 <javierpb275@gmail.com>
2022-01-08 18:12:48 +0100

    prepared methods and routes for user

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -12,6 +12,53 @@ class UsersController {
       return res.status(400).json({ message: error });
     }
   }
+
+  public async login(req: Request, res: Response): Promise<Response> {
+    if (!req.body.email || !req.body.password) {
+      return res
+        .status(400)
+        .json({ message: "Please, Send your email and password" });
+    }
+    try {
+      const foundUser: IUser | null = await User.findOne({
+        email: req.body.email,
+      });
+      if (!foundUser) {
+        return res.status(400).json({ message: "Unable to login" });
+      }
+      return res.status(200).json(foundUser);
+    } catch (error) {
+      return res.status(400).json({ message: "Unable to login" });
+    }
+  }
+
+  public async logout(req: Request, res: Response): Promise<Response> {
+    try {
+      return res.status(200).json({ message: "success logging out" });
+    } catch (error) {
+      return res.status(500).send();
+    }
+  }
+
+  public async getProfile(req: Request, res: Response): Promise<Response> {
+    return res.status(200).json("my user");
+  }
+
+  public async updateProfile(req: Request, res: Response): Promise<Response> {
+    try {
+      return res.status(200).json({ message: "success updating profile" });
+    } catch (error) {
+      return res.status(400).json({ message: error });
+    }
+  }
+
+  public async deleteProfile(req: Request, res: Response): Promise<Response> {
+    try {
+      return res.status(200).json({ message: "success deleting profile" });
+    } catch (error) {
+      return res.status(500).json({ message: error });
+    }
+  }
 }
 
 const usersController = new UsersController();
--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -4,5 +4,10 @@ import usersController from "../controllers/users.controller";
 const router: Router = Router();
 
 router.post("/", usersController.createUser);
+router.post("/login", usersController.login);
+router.post("/logout", usersController.logout);
+router.get("/me", usersController.getProfile);
+router.patch("/me", usersController.updateProfile);
+router.delete("/me", usersController.deleteProfile);
 
 export default router;
