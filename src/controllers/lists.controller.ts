b811d986861fa9838076c9999a0c003a0e149b18
javierpb275 <javierpb275@gmail.com>
2022-01-24 22:05:25 +0100

    getList(), getLists(), createList() and updateList() methods created.

--- src/controllers/lists.controller.ts
+++ src/controllers/lists.controller.ts
@@ -1,33 +1,93 @@
 import { Request, Response } from "express";
+import { IList, IUser } from "../types";
+import User from "../models/user.model";
+import List from "../models/list.model";
+import { getMatch, getPaginationOptions } from "../helpers/paginator.helper";
 
 class ListsController {
   public async getLists(req: Request, res: Response): Promise<Response> {
+    const { userId, query } = req;
     try {
-      return res.status(200).send("getLists");
+      const user: IUser | null = await User.findOne({ _id: userId });
+      if (!user) {
+        return res.status(404).send({ error: "User Not Found!" });
+      }
+      const options = getPaginationOptions(
+        query.limit?.toString(),
+        query.skip?.toString(),
+        query.sort?.toString()
+      );
+      const match = getMatch(query);
+      await user.populate({
+        path: "lists",
+        match,
+        options,
+        populate: { path: "movies", populate: { path: "scores" } },
+      });
+      return res.status(200).send(user.lists);
     } catch (err) {
       return res.status(500).send(err);
     }
   }
 
   public async getList(req: Request, res: Response): Promise<Response> {
+    const { userId, params } = req;
     try {
-      return res.status(200).send("getList");
+      const list: IList | null = await List.findOne({
+        _id: params.id,
+        user: userId,
+      });
+      if (!list) {
+        return res.status(404).send({ error: "List Not Found!" });
+      }
+      await list.populate({ path: "movies", populate: { path: "scores" } });
+      return res.status(200).send(list);
     } catch (err) {
       return res.status(500).send(err);
     }
   }
 
   public async createList(req: Request, res: Response): Promise<Response> {
+    const { userId, body } = req;
     try {
-      return res.status(201).send("createList");
+      const user: IUser | null = await User.findOne({ _id: userId });
+      if (!user) {
+        return res.status(404).send({ error: "User Not Found!" });
+      }
+      const newList: IList = new List({ ...body, user: user._id });
+      await newList.save();
+      user.lists.push(newList._id);
+      await user.save();
+      return res.status(201).send(newList);
     } catch (err) {
       return res.status(400).send(err);
     }
   }
 
   public async updateList(req: Request, res: Response): Promise<Response> {
+    const { userId, body, params } = req;
+    //allow only certain propeties to be updated:
+    const updates: string[] = Object.keys(body);
+    const allowedUpdates: string[] = ["name", "movies"];
+    const isValidOperation: boolean = updates.every((update) =>
+      allowedUpdates.includes(update)
+    );
+    if (!isValidOperation) {
+      return res.status(400).send({ error: "Invalid updates!" });
+    }
+    //-------------------------
     try {
-      return res.status(200).send("updateList");
+      const updatedList: IList | null = await List.findOneAndUpdate(
+        { _id: params.id, user: userId },
+        body,
+        {
+          new: true,
+        }
+      );
+      if (!updatedList) {
+        return res.status(404).send({ error: "List Not Found!" });
+      }
+      return res.status(200).send(updatedList);
     } catch (err) {
       return res.status(400).send(err);
     }
